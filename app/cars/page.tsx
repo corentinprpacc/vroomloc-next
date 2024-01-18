ac6253348d059d3f10a6a1f2eb2aabc84023ffd6
Chloe Moulin <chloe.moulin@accenture.com>
2024-01-18 15:45:05 +0100

    lint --fix

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,50 +1,49 @@
-'use client'
+"use client"
 import CardCar from "../components/card-car"
 import Search from "../components/search"
 import { getAllCars } from "../firebase/utils"
-import { useState, useEffect } from 'react';
-import { Car } from "../firebase/types";
+import { useState, useEffect } from "react"
+import { Car } from "../firebase/types"
 
 interface Props {
-    car: Car[],
-    onDataSend: (data: string) => void;
+  car: Car[]
+  onDataSend: (data: string) => void
 }
 
 const Cars: React.FC<Props> = ({ car }: Props) => {
-    const [cars, setCars] = useState<Car[]>();
-    const [searchCity, setSearchCity] = useState<String>('');
+  const [cars, setCars] = useState<Car[]>()
+  const [searchCity, setSearchCity] = useState<String>("")
 
-    useEffect(() => {
-      async function getCars() {
-        const data = await getAllCars();
-        setCars(data)
-      }
-  
-      getCars();
-    }, []);
+  useEffect(() => {
+    async function getCars() {
+      const data = await getAllCars()
+      setCars(data)
+    }
 
-    const handleInputChange = (inputValue: string) => {
-        setSearchCity(inputValue);
-    };
+    getCars()
+  }, [])
 
-    return (
-        <>
-            <Search getCars={cars} onInputChange={handleInputChange} />
-            <div className="container">
-                <p className="text-center my-4">Résultats</p>
-                <div className="flex cards-block flex-wrap">
-                    {
-                        cars?.filter((car) => 
-                            car.city.toLowerCase().includes(searchCity?.toLowerCase())
-                        ).map((filteredCar, id) => (
-                            <CardCar key={id} getCar={filteredCar}/>
-                        ))
-                    }
-                </div>
-            </div>
-        </>
-    )
+  const handleInputChange = (inputValue: string) => {
+    setSearchCity(inputValue)
+  }
+
+  return (
+    <>
+      <Search getCars={cars} onInputChange={handleInputChange} />
+      <div className="container">
+        <p className="text-center my-4">Résultats</p>
+        <div className="flex cards-block flex-wrap">
+          {cars
+            ?.filter((car) =>
+              car.city.toLowerCase().includes(searchCity?.toLowerCase()),
+            )
+            .map((filteredCar, id) => (
+              <CardCar key={id} getCar={filteredCar} />
+            ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Cars;
-  
+export default Cars
--- app/components/card-car.tsx
+++ app/components/card-car.tsx
@@ -1,26 +1,34 @@
-'use client'
-import Image from 'next/image'
-import { Car } from "../firebase/types";
-import { storage } from '../firebase/config';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+"use client"
+import Image from "next/image"
+import { Car } from "../firebase/types"
+import { storage } from "../firebase/config"
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
 
 interface Props {
-    getCar: Car
+  getCar: Car
 }
 
-const CardCar: React.FC<Props> = ({ getCar }: Props) => { 
-    return (
-        <div className="w-full md:h-full md:max-w-[25%] md:pl-6 mt-8 md:mt-6 md:grow-0">
-            <div className="relative w-full h-auto md:max-h-[25%]">
-                <Image src="/images/car.jpeg" alt="car" width="0" height="0" sizes="100vw" className="w-full h-full" />
-            </div>
-            <div className="mt-4 pl-3 border-l border-solid border-[#c2b2b2]">
-                <p>{getCar.brand} {getCar.model} ({getCar.city})</p>
-                <p className="text-xs">A partir de EUR {getCar.dayPrice} (1 jour)</p>
-            </div>
-        </div>
-    )
+const CardCar: React.FC<Props> = ({ getCar }: Props) => {
+  return (
+    <div className="w-full md:h-full md:max-w-[25%] md:pl-6 mt-8 md:mt-6 md:grow-0">
+      <div className="relative w-full h-auto md:max-h-[25%]">
+        <Image
+          src="/images/car.jpeg"
+          alt="car"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-full"
+        />
+      </div>
+      <div className="mt-4 pl-3 border-l border-solid border-[#c2b2b2]">
+        <p>
+          {getCar.brand} {getCar.model} ({getCar.city})
+        </p>
+        <p className="text-xs">A partir de EUR {getCar.dayPrice} (1 jour)</p>
+      </div>
+    </div>
+  )
 }
 
-export default CardCar;
-  
+export default CardCar
