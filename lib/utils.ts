1191bca5d64853fc5fcb9aa3afeafc533bf0bfd4
corentinparp <corentin.parpette@accenture.com>
2024-01-19 16:43:20 +0100

    feat: adding function to get environnement variables

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getEnv(name: string) {
+  let val = process.env[name]
+  if (val === undefined || val === null) {
+    throw "Missing Env Var for " + name
+  }
+  return val
+}
