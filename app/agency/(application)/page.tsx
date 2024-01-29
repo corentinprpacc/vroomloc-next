59e27231fbc4d9d0e533cd96a2b717330d8b68f7
madi niakate accenture <madi.niakate@accenture.com>
2024-01-29 11:23:35 +0100

    add link to myCars page

--- app/agency/(application)/page.tsx
+++ app/agency/(application)/page.tsx
@@ -11,9 +11,12 @@ export default async function AgencyHome() {
       {session?.user && (
         <div>
           <h1 className="text-3xl">Accueil - Agences</h1>
-          <Link href={`/agency/addCarPage/${session.user.id}`}>
-            Ajout véhicule
-          </Link>
+          <div className="flex space-x-2">
+            <Link href={`/agency/addCarPage/${session.user.id}`}>
+              Ajout véhicule
+            </Link>
+            <Link href="/agency/myCars">Mes voitures</Link>
+          </div>
           <p>Welcome to {session.user.email}</p>
           <div className="bg-black p-2">
             <pre className="w-full text-white p-2 text-wrap">
