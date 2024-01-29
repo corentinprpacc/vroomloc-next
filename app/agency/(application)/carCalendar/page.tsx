fe447d41f4492c340ca92d6f8b0b79f6c9d6127a
madi niakate accenture <madi.niakate@accenture.com>
2024-01-29 11:18:09 +0100

    add calendar page

--- app/agency/(application)/carCalendar/page.tsx
+++ app/agency/(application)/carCalendar/page.tsx
@@ -3,19 +3,21 @@
 import dayGridPlugin from "@fullcalendar/daygrid"
 import interactionPlugin from "@fullcalendar/interaction"
 import FullCalendar from "@fullcalendar/react"
-export default async function carCalendar() {
+export default function carCalendar() {
   function showFunction(e: any) {
     console.log("click on date: ", e.date)
   }
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      events={[
-        { title: "event 1", date: "2024-01-27" },
-        { title: "event 2", date: "2024-01-20" },
-      ]}
-      dateClick={(e) => showFunction(e)}
-    />
+    <div className="w-[50vw]">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          { title: "event 1", date: "2024-01-27" },
+          { title: "event 2", date: "2024-01-20" },
+        ]}
+        dateClick={(e) => showFunction(e)}
+      />
+    </div>
   )
 }
