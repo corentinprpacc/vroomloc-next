a190959c2347597f2f906110b5fefcbc275fc6c1
Chloe Moulin <chloe.moulin@accenture.com>
2024-02-14 15:17:54 +0100

    comment

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -5,6 +5,7 @@ import "swiper/css/navigation"
 import "swiper/css/pagination"
 import Image from "next/image"
 
+// Waiting for dynamic images
 // interface CarouselProps {
 //   slides?: string[]
 // }
